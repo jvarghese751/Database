aa6888f3b6f85ba5a5a2fbfdde31bbc1a045188f
Joel Varghese <Varghese-J2@ulster.ac.uk>
2023-11-24 12:06:13 +0000

    latest

--- app/src/main/java/com/example/database/register.java
+++ app/src/main/java/com/example/database/register.java
@@ -2,12 +2,14 @@ package com.example.database;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class register extends AppCompatActivity {
     EditText name, password, confirm;
@@ -63,6 +65,14 @@ public class register extends AppCompatActivity {
     public static boolean isEmailValid(String email) {
         boolean isValid = false;
 
-        String expression = 
+        String expression = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        CharSequence inputStr = email;
+
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+        if (matcher.matches()) {
+            isValid = true;
+        }
+        return isValid;
     }
 }
